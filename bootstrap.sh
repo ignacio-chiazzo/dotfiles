b7732f2d9c2b8981a736fd1c309304098b185a21
ignacio-chiazzo <ignaciochiazzo@gmail.com>
2021-12-02 12:40:37 -0500

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+echo
+echo
+echo "--- Running bootstrap.sh"
+echo
+echo
+
+# Install Oh My Zsh
+echo "--- Installing OhMyZsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
