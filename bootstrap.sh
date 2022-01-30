573767d32def37c55580c63118a00de212f07ef8
ignacio chiazzo <ignaciochiazzo@ignacios-MacBook-Pro-3.local>
2022-01-29 21:02:55 -0500

    Added config bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,22 @@ echo
 
 # Install Oh My Zsh
 echo "--- Installing OhMyZsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ln -sf ~/dotfiles/configs/.aliases ~/.aliases
 echo "--- ~/dotfiles/configs/.aliases -> ~/.aliases"
+
+# Config files
+echo
+echo "--- Linking dotfiles"
+echo
+
+ln -sf ~/dotfiles/configs/.aliases ~/.aliases
+echo "--- ~/dotfiles/configs/.aliases -> ~/.aliases"
+
+# ZSH
+ln -sf ~/dotfiles/.zlogin ~/.zlogin
+echo "--- ~/dotfiles/.zlogin -> ~/.zlogin"
+
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+echo "--- ~/dotfiles/.zshrc -> ~/.zshrc"
